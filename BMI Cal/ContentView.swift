7f2c70e4452e4b831cf34355e26783fa3fb69249
Mouzzam <moazzamtahir81@gmail.com>
2019-08-19 17:15:41 +0500

    Initial Commit

---
+++ BMI Cal/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BMI Cal
+//
+//  Created by Moazzam Tahir on 19/08/2019.
+//  Copyright © 2019 Moazzam Tahir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
